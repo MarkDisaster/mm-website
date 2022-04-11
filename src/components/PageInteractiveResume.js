79eb62d96c762984dba990061c3e3a15bd417aba
Marek Marek <marek.marek@hotmail.cz>
2022-04-11 14:55:44 +0200

    Vytvoření stránky Interactive Resume.

---
+++ src/components/PageInteractiveResume.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function PageInteractiveResume() {
+
+   /*setTimeout(function() {
+      document.querySelector('.header-container').classList.add('center');
+   }, 1000);*/
+
+
+   return (
+      <>
+         <div className="page page-1">
+            <div className="header-container">
+               <h1>Marek</h1>
+               <h1>Marek</h1>
+               <h2>Interactive Resume</h2>
+            </div>
+         </div>
+         <div className="page page-2">
+            Page 2
+         </div>
+
+      </>
+   )
+}
+
+export default PageInteractiveResume
